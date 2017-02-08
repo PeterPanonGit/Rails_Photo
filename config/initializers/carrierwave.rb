854d6dbecc441952e553a09e65a5ca13d36e5234
huapu <huapu@huapu-ThinkPad-L430>
2017-02-07 23:39:51 -0500

    do not ask for AWS credentials in dev mode

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,14 +1,13 @@
 # config/initializers/carrierwave.rb
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-   # Configuration for Amazon S3
-    :provider              => 'AWS',
-    :aws_access_key_id     => ENV['S3_KEY'],
-    :aws_secret_access_key => ENV['S3_SECRET'],
-    :region                => ENV['S3_REGION']
-  }
-
   if Rails.env.production?
+    config.fog_credentials = {
+     # Configuration for Amazon S3
+      :provider              => 'AWS',
+      :aws_access_key_id     => ENV['S3_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET'],
+      :region                => ENV['S3_REGION']
+    }
     config.storage = :fog
   else
     config.storage = :file
