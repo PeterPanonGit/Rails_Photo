c9044921547a6e0fab4e9feba9ce8a5c3333ec68
OneOfCats <krableshtraus@gmail.com>
2017-02-22 20:01:04 +0200

    allowing only one job per worker

--- app/jobs/resque_job.rb
+++ app/jobs/resque_job.rb
@@ -9,10 +9,11 @@ class ResqueJob
 
   def self.perform(*arg)
     #return false if arg.blank? || arg[0].blank?
-
+    `kill -s USR2 $(head -n 1 #{ENV['RESQUE_PID_FILE']})`
     img_job = ImageJob.new(@queue)
     #img_job.set_config()
     img_job.execute
+    `kill -s CONT $(head -n 1 #{ENV['RESQUE_PID_FILE']})`
   end
 
 end
